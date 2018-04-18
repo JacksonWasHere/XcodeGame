77e1531938906a92b84e544558eefd06cce7decb
Mobile <mobile@rm26imac027.local>
2018-04-18 09:07:32 -0700

    Stage 1

--- BrEaaaK BroKK/GameScene.swift
+++ BrEaaaK BroKK/GameScene.swift
@@ -12,22 +12,35 @@ import GameplayKit
 class GameScene: SKScene,SKPhysicsContactDelegate {
     
     var paddle:SKSpriteNode!
+    var ball:SKSpriteNode!
     
-    let rows = 1
-    let perRow = 4
+    let rows = 4
+    let perRow = 5
     let width = 500
     let height = 300
     
+    var cam: SKCameraNode?
+    
     override func didMove(to view: SKView) {
         scene?.physicsBody = SKPhysicsBody(edgeLoopFrom: self.frame)
+        self.physicsBody?.linearDamping=0
+        self.physicsBody?.friction=0
+        self.physicsBody?.angularDamping=0
+        self.physicsBody?.restitution=1
+        self.physicsBody?.isDynamic=false
         
+        cam = SKCameraNode()
+        self.camera = cam
+        self.addChild(cam!)
+        
+        ball = self.childNode(withName: "ball") as! SKSpriteNode
         paddle = self.childNode(withName: "paddle") as! SKSpriteNode
         
         for i in 0..<rows {
             for j in 0..<perRow {
-                let spacingX = width/perRow
-                let spacingY = height/rows
-                spawnBrick(x: CGFloat(spacingX * (j))-245, y: CGFloat(spacingY * i)+552)
+                let spacingX = width/(perRow-1)
+                let spacingY = height/(rows)
+                spawnBrick(x: CGFloat(spacingX * (j))-245, y: -CGFloat(spacingY * i)+552)
             }
         }
     }
@@ -41,7 +54,13 @@ class GameScene: SKScene,SKPhysicsContactDelegate {
     }
     
     func didBegin(_ contact: SKPhysicsContact) {
-        
+//        var bodyA = contact.bodyA.node?.name=="paddle" ? contact.bodyA : contact.bodyB
+//        var bodyB = contact.bodyA.node?.name=="paddle" ? contact.bodyB : contact.bodyA
+//        var distance = (bodyA.node?.position.x)! - (bodyB.node?.position.x)!
+//        var speed = distance*5
+//        print(speed)
+//        bodyB.node?.physicsBody?.velocity = CGVector(dx: speed, dy: 200)
+//
     }
     
     func touchDown(atPoint pos : CGPoint) {
